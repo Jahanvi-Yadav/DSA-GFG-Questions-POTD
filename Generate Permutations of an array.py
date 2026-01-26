be1e50efdc794f2d0fe4c29cb94045d026c9c15d
Jahanvi-Yadav <jahanviy1@gmail.com>
2026-01-26 08:33:48 +0530

    Update Generate Permutations of an array.py

--- Generate Permutations of an array.py
+++ Generate Permutations of an array.py
@@ -1,1 +1,22 @@
-
+#Generate Permutations of an array. 
+#GFG POTD using Python.
+class Solution:
+    def permuteDist(self, arr):
+        n = len(arr)
+        used = [False] * n
+        
+        def dfs(acc: list[int]=[], output: list[int]=[]) -> list[int]:
+            if len(acc) == n:
+                output.append(acc.copy())
+                return output
+            for i in range(n):
+                if used[i]:
+                    continue
+                used[i] = True
+                acc.append(arr[i])
+                dfs()
+                acc.pop()
+                used[i] = False
+            return output
+        
+        return dfs()
