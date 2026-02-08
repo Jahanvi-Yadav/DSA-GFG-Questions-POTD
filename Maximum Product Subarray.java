00d289bf06fe9181cf412a9a19599bb430480dff
Jahanvi-Yadav <jahanviy1@gmail.com>
2026-02-08 23:25:54 +0530

    Update and rename Maximum Product Subarray to Maximum Product Subarray.java

--- Maximum Product Subarray.java
+++ Maximum Product Subarray.java
@@ -1,5 +1,5 @@
-#Maximum Product Subarray
-#Given an array arr[] that contains positive and negative integers (may contain 0 as well). Find the maximum product that we can get in a subarray of arr[].
+//Maximum Product Subarray
+//Given an array arr[] that contains positive and negative integers (may contain 0 as well). Find the maximum product that we can get in a subarray of arr[].
 
 #Note: It is guaranteed that the answer fits in a 32-bit integer.
 class Solution {
