b1a4007d4988e336ea20ca96edfc238664b72f30
Jahanvi-Yadav <jahanviy1@gmail.com>
2026-02-10 13:52:37 +0530

    Update Koko Eating Bananas.py

--- Koko Eating Bananas.py
+++ Koko Eating Bananas.py
@@ -1,1 +1,12 @@
+'''Koko is given an array arr[], where each element represents a pile of bananas. She has exactly k hours to eat all the bananas.
+
+Each hour, Koko can choose one pile and eat up to s bananas from it.
+
+If the pile has atleast s bananas, she eats exactly s bananas.
+If the pile has fewer than s bananas, she eats the entire pile in that hour.
+
+Koko can only eat from one pile per hour.
+
+
+Your task is to find the minimum value of s (bananas per hour) such that Koko can finish all the piles within k hours.'''
 
