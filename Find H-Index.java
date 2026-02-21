c8deffca5785f4bcaeadc99d56325f0345a46793
Jahanvi-Yadav <jahanviy1@gmail.com>
2026-02-21 14:50:08 +0530

    Update and rename Find H-Index to Find H-Index.java

---
+++ Find H-Index.java
@@ -0,0 +1,3 @@
+/* You are given an array citations[], where each element citations[i] represents the number of citations received by the ith paper of a researcher. You have to calculate the researcher’s H-index.
+The H-index is defined as the maximum value H, such that the researcher has published at least H papers, and all those papers have citation value greater than or equal to H. */
+
